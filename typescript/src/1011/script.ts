73b6c5daf671ee35d1afab839ca0ffb6a530482b
Matheus Santos Lopes <matthewpeterparker@gmail.com>
2024-10-25 11:25:02 -0300

    idk

---
+++ typescript/src/1011/script.ts
@@ -0,0 +1,12 @@
+import * as readline from "readline";
+import { stdin as input} from "process"
+
+const readLine: readline.Interface = createInterface({ input });
+
+readLine.question("", () => {
+   
+
+
+ readLine.close();
+})
+
