f757fb8dc505b1f34b9281f3ba4a1b9176831ac2
Matheus Santos Lopes <matthewpeterparker@gmail.com>
2024-10-25 13:45:19 -0300

    solving the 1011

--- typescript/src/1011/script.ts
+++ typescript/src/1011/script.ts
@@ -1,12 +1,15 @@
 import * as readline from "readline";
 import { stdin as input} from "process"
 
-const readLine: readline.Interface = createInterface({ input });
+const readLine: readline.Interface = readline.createInterface({ input });
 
-readLine.question("", () => {
-   
-
+readLine.question("", (input:string) => {
+  let lines: string[] = input.split(/\s+/);  
+  let r: number = Number(lines[0]); 
+  let PI: number = 3.14159;
+  let raio: number = (((4.0/3) * PI) * Math.pow(r,3));  
+  console.log(`VOLUME = ${raio.toFixed(3)}`);
 
  readLine.close();
-})
+});
 
