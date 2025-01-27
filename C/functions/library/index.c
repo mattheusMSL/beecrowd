4077ce89365b6b27631457461d0d2186990883e8
Matheus Santos Lopes <matthewpeterparker@gmail.com>
2025-01-27 10:03:31 -0300

    change file name main to index

---
+++ C/functions/library/index.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "somai.h"
+
+int main(){
+  int a = 5, b = 3;
+  int res = soma(a, b);
+  printf("%d\n", res);
+
+}
--- C/functions/library/main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include "somai.h"
-
-int main(){
-  int numberOne = 5, numberTwo = 3;
-  int res = soma(numberOne, numberTwo);
-  printf("%d\n", res);
-
-}
